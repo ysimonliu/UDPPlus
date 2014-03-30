8348396533d4bc07ca5475f382805cea9728ceb4
mjval_lin laptop win7 <mjval_lin@hotmail.com>
2014-03-29 22:02:29 -0400

    added guice library to build path

--- src/communication/Transmitter.java
+++ src/communication/Transmitter.java
@@ -1,5 +1,20 @@
 package communication;
 
-public class Transmitter {
+import java.net.DatagramSocket;
 
+import com.google.inject.Inject;
+
+import forwardErrorCorrection.FECInterface;
+
+public class Transmitter {
+	private DatagramSocket socket;
+	private FECInterface fec;
+
+	@Inject
+	public Transmitter(DatagramSocket socket, FECInterface fec) {
+		this.socket = socket;
+		this.fec = fec;
+	}
+	
+		
 }
--- src/communication/receiver.java
+++ src/communication/receiver.java
@@ -1,5 +1,19 @@
 package communication;
 
+import java.net.DatagramSocket;
+
+import com.google.inject.Inject;
+
+import forwardErrorCorrection.FECInterface;
+
 public class Receiver {
 
+	private DatagramSocket socket;
+	private FECInterface fec;
+
+	@Inject
+	public Receiver(DatagramSocket socket, FECInterface fec) {
+		this.socket = socket;
+		this.fec = fec;
+	}
 }
