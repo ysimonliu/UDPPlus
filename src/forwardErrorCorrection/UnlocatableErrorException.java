fbc150e9f670fde1dba956d080d3fd7f0ce8a0a7
mjval_lin laptop win7 <mjval_lin@hotmail.com>
2014-03-31 11:56:42 -0400

    added forgotten Exception class

---
+++ src/forwardErrorCorrection/UnlocatableErrorException.java
@@ -0,0 +1,14 @@
+package forwardErrorCorrection;
+
+public class UnlocatableErrorException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5913580002982489428L;
+
+	public UnlocatableErrorException(String message) {
+		super(message);
+	}
+
+}
