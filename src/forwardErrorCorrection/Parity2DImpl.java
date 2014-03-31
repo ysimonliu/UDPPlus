07db86fe56553b0af824d158b266ce9008d06d2a
mjval_lin laptop win7 <mjval_lin@hotmail.com>
2014-03-31 17:57:40 -0400

    added more unit tests for 2D Parity2DImpl.java

--- src/forwardErrorCorrection/Parity2DImpl.java
+++ src/forwardErrorCorrection/Parity2DImpl.java
@@ -132,7 +132,6 @@ public class Parity2DImpl implements FECInterface {
 			} 
 		}
 		
-		
 		return sb.toString();
 	}
 	
--- test/forwardErrorCorrection/Parity2DImplTest.java
+++ test/forwardErrorCorrection/Parity2DImplTest.java
@@ -296,12 +296,74 @@ public class Parity2DImplTest {
 	}
 	
 	@Test
-	public void testDecodeLowerCase2BitError() {
+	public void testDecodeLowerCase2BitErrorSameRow() {
 		
+		int h = Integer.parseInt("11010000", 2);
+		int e = Integer.parseInt("11111011", 2);
+		int l = Integer.parseInt("11011001", 2);
+		int o = Integer.parseInt("11011111", 2);
+		int last = Integer.parseInt("00011101", 2);;
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append((char)h);
+		sb.append((char)e);
+		sb.append((char)l);
+		sb.append((char)l);
+		sb.append((char)o);
+		sb.append((char)last);
+		
+		try {
+			parity2D.decode(sb.toString());
+			fail("UnlocatableErrorException not thrown. i.e.: errors not detected!");
+		} catch (UnlocatableErrorException e1) {
+		}
 	}
 	
 	@Test
-	public void testDecodeMixedCase2BitError() {
+	public void testDecodeMixedCase2BitErrorSameRow() {
+		int H = Integer.parseInt("10010001", 2);
+		int E = Integer.parseInt("10001010", 2);
+		int l = Integer.parseInt("11011001", 2);
+		int L = Integer.parseInt("10011000", 2);
+		int o = Integer.parseInt("11101111", 2);
+		int last = Integer.parseInt("00111101", 2);;
 		
+		StringBuilder sb = new StringBuilder();
+		sb.append((char)H);
+		sb.append((char)E);
+		sb.append((char)l);
+		sb.append((char)L);
+		sb.append((char)o);
+		sb.append((char)last);
+		
+		try {
+			parity2D.decode(sb.toString());
+			fail("UnlocatableErrorException not thrown. i.e.: errors not detected!");
+		} catch (UnlocatableErrorException e1) {
+		}
+	}
+	
+	@Test
+	public void testDecodeMixedCase2BitErrorDiffRow() {
+		int H = Integer.parseInt("10010001", 2);
+		int E = Integer.parseInt("10001010", 2);
+		int l = Integer.parseInt("11111001", 2);
+		int L = Integer.parseInt("10111000", 2);
+		int o = Integer.parseInt("11011111", 2);
+		int last = Integer.parseInt("00111101", 2);
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append((char)H);
+		sb.append((char)E);
+		sb.append((char)l);
+		sb.append((char)L);
+		sb.append((char)o);
+		sb.append((char)last);
+		
+		try {
+			parity2D.decode(sb.toString());
+			fail("UnlocatableErrorException not thrown. i.e.: errors not detected!");
+		} catch (UnlocatableErrorException e1) {
+		}
 	}
 }
