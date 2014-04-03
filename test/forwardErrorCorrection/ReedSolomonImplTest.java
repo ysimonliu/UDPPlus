3c2ed1691daafdbf63a6bf41f952b0a09350699f
Simon Liu <ysimonliu@gmail.com>
2014-04-03 10:46:16 -0400

    Added two test cases, and to be continued at school for the rest of the testings

--- test/forwardErrorCorrection/ReedSolomonImplTest.java
+++ test/forwardErrorCorrection/ReedSolomonImplTest.java
@@ -1,6 +1,7 @@
 package forwardErrorCorrection;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -17,16 +18,19 @@ public class ReedSolomonImplTest {
 	}
 	
 	@Test
-	public void testEncodeUpperCase(){
+	public void testEncodeUpperCase() {
 		String message = "HELLO";
-		System.out.println("DEBUG: original message: " + message);
 		String encoded = rsImpl.encode(message);
-		System.out.println("DEBUG: encoded message: " + encoded);
-		try {
-			String decoded = rsImpl.decode(encoded);
-			System.out.println("DEBUG: decoded message: " + decoded);
-		} catch (Exception ex) {
-			System.out.print("Exception when decoding: " + ex);
-		}
+		String expected = "72, 69, 76, 76, 79, 168, 254, 255";
+		assertEquals(expected, encoded);
 	}
+	
+	@Test
+	public void testEncodeLowerCase() {
+		String message = "hello";
+		String encoded = rsImpl.encode(message);
+		String expected = "104, 101, 108, 108, 111, 98, 199, 222";
+		assertEquals(expected, encoded);
+	}
+	
 }
