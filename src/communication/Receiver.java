c231962876ac760ebbb4ad322f96b24b0ffcac33
Simon Liu <ysimonliu@gmail.com>
2014-04-03 15:18:10 -0400

    Simple transmitter and receiver implemented, more work needed for receiver

--- src/communication/Receiver.java
+++ src/communication/Receiver.java
@@ -1,19 +1,29 @@
 package communication;
 
-import java.net.DatagramSocket;
+import java.net.DatagramPacket;
 
-import com.google.inject.Inject;
+import reedSolomon.ReedSolomonException;
 
-import forwardErrorCorrection.FECInterface;
+import forwardErrorCorrection.ReedSolomonImpl;
+import forwardErrorCorrectionException.NotAsciiCodeExpcetion;
 
 public class Receiver {
 
-	private DatagramSocket socket;
-	private FECInterface fec;
-
-	@Inject
-	public Receiver(DatagramSocket socket, FECInterface fec) {
-		this.socket = socket;
-		this.fec = fec;
+	public static void main(String[] args){
+		
+		ReedSolomonImpl rsImpl = new ReedSolomonImpl(Transmitter.EC_BYTES);
+		
+		while (true) {
+    	  //Receive reversed message from server
+    	  byte[] inBuf = new byte[256];
+    	  DatagramPacket inPacket = new DatagramPacket(inBuf, inBuf.length);
+    	  String data = new String(inPacket.getData(), 0, inPacket.getLength());
+    	  System.out.println("Received data from Transmitter : " + data);
+    	  try {
+    		  System.out.println("Decoded data: " + rsImpl.decode(data));
+    	  } catch (ReedSolomonException | NotAsciiCodeExpcetion e) {
+    		  System.out.println("Receiver Failed to decode the data using Reed Solomon");
+    	  }
+		}
 	}
 }
--- src/communication/Transmitter.java
+++ src/communication/Transmitter.java
@@ -1,20 +1,37 @@
 package communication;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-
-import com.google.inject.Inject;
-
+import java.net.InetAddress;
+import java.net.SocketException;
 import forwardErrorCorrection.FECInterface;
 
 public class Transmitter {
+	
+	public static final int EC_BYTES = 3;
 	private DatagramSocket socket;
+	private byte[] outBuf;
+	private int port;
 	private FECInterface fec;
-
-	@Inject
-	public Transmitter(DatagramSocket socket, FECInterface fec) {
-		this.socket = socket;
+	
+	public Transmitter(int port, FECInterface fec) throws SocketException {
+		this.socket = new DatagramSocket();
+		this.outBuf = new byte[1024];
+		this.port = port;
 		this.fec = fec;
 	}
 	
-		
+	public void send(String msg) throws IOException{
+		try {
+			InetAddress IPAddress = InetAddress.getByName("localhost");
+			String encoded = fec.encode(msg);
+			System.out.println("Encoded message is: " + encoded);
+			outBuf = encoded.getBytes();
+			DatagramPacket outPacket = new DatagramPacket(outBuf, outBuf.length, IPAddress, port);
+			socket.send(outPacket);
+		} catch (IOException ex) {
+			System.out.println("Transmitter failed to send message");
+		}
+	}
 }
